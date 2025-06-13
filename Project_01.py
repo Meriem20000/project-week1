3c5f811b807e318667597318f286309f3d27548a
Meriem20000 <148897815+Meriem20000@users.noreply.github.com>
2025-06-13 16:56:39 +0100

    Add files via upload

--- Project_01.py
+++ Project_01.py
@@ -11,13 +11,14 @@ days_lived =  int(age) * 365
 next_age =  int(age) + 1
 
 print("\n YOUR PROFILE \n")
-print("Name: " + first_name + " " + last_name)
-print("Age: " + age + " years old ")
-print("Location: " + city )
-print("Hobby: " + hobby )
-print("Birth Year: " + str(Birth_year))
-print("Days Lived: " + str(days_lived)+" days" )
-print("Next Birthday: You'll be " +str(next_age) + "! \n " )
+print(f"Name: {first_name } {last_name}")
+
+print(f"Age: {age} years old ")
+print(f"Location: { city  }")
+print(f"Hobby: {hobby }" )
+print(f"Birth Year: {Birth_year}")
+print(f"Days Lived: {days_lived} days" )
+print(f"Next Birthday: You'll be {next_age}! \n " )
 
 print(" Welcome to Python programming, Alice!")
 
